86cce0c91ab2517e9798228fa88507f026b047f7
OKUMURA Takahiro <hfm.garden@gmail.com>
2016-02-18 00:12:59 +0900

    Test compilation

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 describe 'serf' do
 
   context 'with defaults for all parameters' do
+    it { is_expected.to compile }
+    it { is_expected.to compile.with_all_deps }
     it { should contain_class('serf') }
   end
 end
